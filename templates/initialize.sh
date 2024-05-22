0c21fd2e7d32fbd8b92d380e9ea8e099089e50df
Oleksandr Polishchuk <oleksandr@livewyer.com>
2024-05-22 20:08:22 +0300

    feat: upload comparison results Istio Sidecar vs Istio Ambient
    upload new templates
    
    upload second round of tests Istio Sidecar vs Istio Ambient
    
    rename folder with the results, upload test report tables
    
    fix tables
    
    setup structure, small fixes
    
    finish section 1
    
    section 2, feedback
    
    add diagrams
    
    minor fixes
    
    add summary
    
    adjust image size
    
    upload templates
    
    add operational manual
    
    minor fixes
    
    minor fixes
    
    minor fixes
    
    minor fixes
    
    feedback
    
    Update test-report-istio.md
    
    move graphs
    
    Update test-report-istio.md
    
    Update test-report-istio.md

--- templates/initialize.sh
+++ templates/initialize.sh
@@ -5,7 +5,7 @@
 # Safe scripting
 set -euo pipefail
 
-CLUSTER_NAME='-service-mesh-poc'
+CLUSTER_NAME=
 CILIUM_API_SERVER_IP=
 
 initialize() {
@@ -15,8 +15,11 @@ initialize() {
 
   # Add user and fetch kubeconfig
   echo "Fetching kubeconfig..."
+  
   kubectl --namespace="$CLUSTER_NAME" get secret "$CLUSTER_NAME-kubeconfig" -o jsonpath='{.data.value}' | base64 --decode >managed-test.kubeconfig
   kubectl --kubeconfig managed-test.kubeconfig patch configmap -n kube-system aws-auth -p '{"data":{"mapUsers":"[{\"userarn\": \"arn:aws:iam::000000000000:user/lw-oleksandr\", \"username\": \"lw-oleksandr\", \"groups\": [\"system:masters\"]}, {\"userarn\": \"arn:aws:iam::000000000000:user/lw-walter\", \"username\": \"lw-walter\", \"groups\": [\"system:masters\"]}]"}}'
+  
+  CLUSTER_NAME='istio-ambient-service-mesh-poc'
   aws eks update-kubeconfig --name "$CLUSTER_NAME"_"$CLUSTER_NAME"-control-plane
 
   # Patch AWS Node DS
@@ -50,21 +53,58 @@ restart-pods() {
 prerequisites() {
   initialize
   taints
-  #restart-pods
+  restart-pods
 }
 
 # Deploy service mesh
-istio() {
+istio_ambient() {
   # Istio
   echo "Starting Istio deployment..."
   helm repo add istio https://istio-release.storage.googleapis.com/charts
   helm repo update
 
   echo "Installing Istio CRDs..."
-  helm upgrade --install istio-base istio/base -n istio-system --create-namespace --set defaultRevision=default
+  helm upgrade --install istio-base istio/base -n istio-system --create-namespace --set defaultRevision=default --version 1.22.0 
+
+  echo "Installing Istio CNI..."
+  helm upgrade --install istio-cni istio/cni -n istio-system --set profile=ambient --wait --version 1.22.0
 
   echo "Installing Istio Discovery Service..."
-  helm upgrade --install istiod istio/istiod -n istio-system --wait \
+  helm upgrade --install istiod istio/istiod -n istio-system --set profile=ambient --wait --version 1.22.0 \
+    --set defaults.pilot.tolerations[0].key=type \
+    --set-string defaults.pilot.tolerations[0].value=service-mesh \
+    --set defaults.pilot.tolerations[0].operator=Equal \
+    --set defaults.pilot.tolerations[0].effect=NoSchedule \
+    --set defaults.pilot.resources.requests.memory=1024Mi
+
+  echo "Installing Istio ztunnel..."
+  helm upgrade --install ztunnel istio/ztunnel -n istio-system --wait --version 1.22.0 \
+    --set defaults.resources.requests.memory=1024Mi
+
+  kubectl wait --for=condition=Ready pods --all -n istio-system
+
+  # Can be disabled for some cases
+  echo "Installing Istio Gateway..."
+  helm install istio-ingress istio/gateway -n istio-ingress --create-namespace --wait --version 1.22.0 \
+    --set defaults.tolerations[0].key=type \
+    --set-string defaults.tolerations[0].value=service-mesh \
+    --set defaults.tolerations[0].operator=Equal \
+    --set defaults.tolerations[0].effect=NoSchedule
+
+  kubectl wait --for=condition=Ready pods --all -n istio-ingress
+}
+
+istio_sidecar() {
+  # Istio
+  echo "Starting Istio deployment..."
+  helm repo add istio https://istio-release.storage.googleapis.com/charts
+  helm repo update
+
+  echo "Installing Istio CRDs..."
+  helm upgrade --install istio-base istio/base -n istio-system --create-namespace --set defaultRevision=default --version 1.22.0 
+
+  echo "Installing Istio Discovery Service..."
+  helm upgrade --install istiod istio/istiod -n istio-system --wait --version 1.22.0 \
     --set defaults.pilot.tolerations[0].key=type \
     --set-string defaults.pilot.tolerations[0].value=service-mesh \
     --set defaults.pilot.tolerations[0].operator=Equal \
@@ -72,9 +112,8 @@ istio() {
   kubectl wait --for=condition=Ready pods --all -n istio-system
 
   # Can be disabled for some cases
-
   echo "Installing Istio Gateway..."
-  helm install istio-ingress istio/gateway -n istio-ingress --create-namespace --wait \
+  helm install istio-ingress istio/gateway -n istio-ingress --create-namespace --wait --version 1.22.0 \
     --set defaults.tolerations[0].key=type \
     --set-string defaults.tolerations[0].value=service-mesh \
     --set defaults.tolerations[0].operator=Equal \
@@ -107,14 +146,12 @@ cilium() {
 
   echo "Upgrading Cilium deployment..."
   # Value of API_SERVER_IP variable is public IP or hostname of your API Server
-  API_SERVER_IP=$CILIUM_API_SERVER_IP
-  API_SERVER_PORT=443
-  helm upgrade --install cilium cilium/cilium --version 1.15.1 --values templates/tolerations.yaml \
+  API_SERVER_IP=127.0.0.1
+  API_SERVER_PORT=36057
+  helm upgrade --install cilium cilium/cilium --version 1.15.1 \
     --namespace kube-system \
-    --set eni.enabled=true \
-    --set ipam.mode=eni \
-    --set egressMasqueradeInterfaces=eth0 \
-    --set routingMode=native \
+    --set image.pullPolicy=IfNotPresent \
+    --set ipam.mode=kubernetes \
     --set kubeProxyReplacement=true \
     --set k8sServiceHost=${API_SERVER_IP} \
     --set k8sServicePort=${API_SERVER_PORT} \
@@ -175,14 +212,19 @@ linkerd() {
 }
 
 # Define list of arguments expected in the input
-optstring="icl"
+optstring="ascl"
 
 # Get the options (arguments)
 while getopts "$optstring" arg; do
   case $arg in
-  i)
+  a)
     prerequisites
-    istio
+    istio_ambient
+    exit 0
+    ;;
+  s)
+    prerequisites
+    istio_sidecar
     exit 0
     ;;
   c)
